f4a11c0c3ab016f5cbab7bae681a58633aea1039
Pawel Barcik <pawel.barcik@gmail.com>
2013-06-04 17:20:39 +0100

    refactor PropertySearch slightly to perform searches from an instance not a class

--- lib/property_service/property_search.rb
+++ lib/property_service/property_search.rb
@@ -1,21 +1,17 @@
 module PropertyService
   class PropertySearch
-    class << self
+    def find_similar_properties(options)
+      property_data_supplier.find_similar_properties(options)
+    end
 
-      def find_similar_properties(options)
-        property_data_supplier.find_similar_properties(options)
-      end
+    def find_in_proximity(options)
+      property_data_supplier.find_in_proximity(options)
+    end
 
-      def find_in_proximity(options)
-        property_data_supplier.find_in_proximity(options)
-      end
+    private
 
-      private
-
-      def property_data_supplier
-        @property_data_supplier ||= ::PropertyService::DataSuppliers::PropertyData.new
-      end
-
+    def property_data_supplier
+      @property_data_supplier ||= ::PropertyService::DataSuppliers::PropertyData.new
     end
   end
 end
--- spec/property_service/property_search_spec.rb
+++ spec/property_service/property_search_spec.rb
@@ -2,6 +2,33 @@ require "spec_helper"
 
 describe PropertyService::PropertySearch do
 
-  let(:klass) { PropertyService::PropertySearch }
+  let(:instance) { PropertyService::PropertySearch.new }
+  let(:property_data_supplier) { stub({}) }
+  let(:search_params) { stub({}) }
+  let(:search_results) { stub({}) }
+
+  before do
+    PropertyService::DataSuppliers::PropertyData.stub(:new).and_return(
+      property_data_supplier
+    )
+  end
+
+  context "#find_in_proximity" do
+    it "delegates search correctly" do
+      property_data_supplier.should_receive(:find_in_proximity)
+        .with(search_params).and_return(search_results)
+
+      expect(instance.find_in_proximity(search_params)).to eq search_results
+    end
+  end
+
+  context "#find_similar_properties" do
+    it "delegates search correctly" do
+      property_data_supplier.should_receive(:find_similar_properties)
+        .with(search_params).and_return(search_results)
+
+      expect(instance.find_similar_properties(search_params)).to eq search_results
+    end
+  end
 
 end
